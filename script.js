abb31b6cbc30fec14bd859e1c4e108b96ad1152c
Francesco Moles <francesco.moles@outlook.it>
2022-03-17 21:06:54 +0100

    when player lose show bombs and stop clicking add a points counter

--- script.js
+++ script.js
@@ -1,58 +1,79 @@
-/*creare una griglia di gioco quadrata, in cui ogni cella contiene un 
-numero tra quelli compresi in un range compreso tra 1 e 100
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
+const playBtn = document.getElementById("play");
+playBtn.addEventListener("click", () => startGame());
 
+function startGame() {
+    createGrid();
+    clickEvent();
+}
 
+function difficulty() {
+    const selectValue = document.getElementById("difficulty");
+    if (selectValue.selectedIndex == 0) {
+        num = 100;
+        level = "lvlEasy";
+    }
+    else if (selectValue.selectedIndex == 1) {
+        num = 81;
+        level = "lvlMed";
+    }
+    else {
+        num = 49;
+        level = "lvlHard";
+    }
+    return { num, level };
+}
 
-const btnEasy = document.getElementById("easy");
-const btnMed = document.getElementById("med");
-const btnHard = document.getElementById("hard");
-
-btnEasy.addEventListener("click", () => playGrid(100, "lvlEasy"));
-btnMed.addEventListener("click", () => playGrid(81, "lvlMed"));
-btnHard.addEventListener("click", () => playGrid(49, "lvlHard"));
-
-
-function playGrid(maxCells, level) {
+function createGrid() {
+    const maxCells = difficulty().num;
+    const levelName = difficulty().level;
     const gridBox = document.getElementById("box");
     gridBox.innerHTML = "";
-    const bombNum = bomb(maxCells);
+    gridBox.classList.remove("lose");
     for (let i = 0; i < maxCells; i++) {
-        const cells = createCells();
-        cells.classList.add(level);
-        cells.innerHTML = i + 1;
-        gridBox.appendChild(cells);
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.classList.add(levelName);
+        cell.innerHTML = i + 1;
+        gridBox.appendChild(cell);
+    }
+}
 
-        cells.addEventListener("click", function () {
+function clickEvent() {
+    const bombNumbers = createBombs();
+    console.log(bombNumbers);
+    const totalCell = document.querySelectorAll(".cell");
+    const gridBox = document.getElementById("box");
+    let points = 0;
+    let clicked = [];
 
-            if (bombNum.includes(i + 1)) {
-                cells.classList.add("bg-red");
-                alert("Hai perso");
-                gridBox.innerHTML = "";
+    for (let i = 0; i < totalCell.length; i++) {
+        totalCell[i].addEventListener("click", function () {
+            if (bombNumbers.includes(i + 1)) {
+                showAllBombs(bombNumbers);
+                gridBox.classList.add("lose");
+                alert("Hai perso! Sei riuscito a fare: " + points + " punti!");
             }
             else {
-                cells.classList.add("bg-click");
+                if (!clicked.includes(i + 1)) {
+                    clicked.push(i + 1);
+                    if (points != totalCell.length - 17) {
+                        totalCell[i].classList.add("bg-click");
+                        points++;
+                    }
+                    else {
+                        youWin();
+                    }
+                }
             }
-
         })
     }
-
-    console.log(bombNum);
-
-
 }
 
-function createCells() {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    return cell;
-}
-
-function bomb(lvlCells) {
+function createBombs() {
     let randNumber = [];
     let num = 0;
     for (let i = 0; i < 16; i++) {
-        num = (Math.floor(Math.random() * lvlCells) + 1);
+        num = (Math.floor(Math.random() * difficulty().num) + 1);
         if (!randNumber.includes(num)) {
             randNumber.push(num);
         }
@@ -63,3 +84,15 @@ function bomb(lvlCells) {
     return randNumber;
 }
 
+function showAllBombs(allBomb) {
+    const totalCells = document.querySelectorAll(".cell");
+    for (let i = 0; i < totalCells.length; i++) {
+        if (allBomb.includes(i + 1)) {
+            totalCells[i].classList.add("bg-red");
+        }
+    }
+}
+
+function youWin() {
+    alert("Hai vinto");
+}
